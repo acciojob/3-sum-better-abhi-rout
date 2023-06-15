68ef12dc2604dd9eea23037d39c4bbd198f0a056
abhi-rout <130905508+abhi-rout@users.noreply.github.com>
2023-06-15 18:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ function threeSum(arr, target) {
 	let n=arr.length;
 	let ans=Number.MAX_VALUE
 	for(let i=0;i<n-2;i++){
-		target=target-arr[i];
+		currentTarget=target-arr[i];
 		let j=i+1;
 	    let k=n-1;
 		let sum=0;
 	    while(j<k){
 			sum=arr[j]+arr[k];
-			ans=  Math.min(ans,Math.abs(target-sum))
+			ans=  Math.min(ans,Math.abs(currentTarget-sum))
 			
-			if(sum>target){
+			if(sum>currentTarget){
 				k--;
 			}
-			else if(sum<target){
+			else if(sum<currentTarget){
 				j++;
 			}
-			else if(sum===target){
+			else if(sum===currentTarget){
 				return sum;
 			}
 		}
