23c74fdb64a63203155da6e1bc07e3eeb0f19b77
abhi-rout <130905508+abhi-rout@users.noreply.github.com>
2023-06-15 16:49:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function threeSum(arr, target) {
-// write your code here
+	arr.sort();
+	let n=arr.length;
+	for(let i=0;i<n-2;i++){
+		target=target-arr[i];
+		let j=i+1;
+	    let k=n-1;
+		let sum=0;
+	    while(j<k){
+			sum=arr[j]+arr[k];
+			
+			if(sum>target){
+				k--;
+			}
+			else if(sum<target){
+				j++;
+			}
+			else if(sum===target){
+				return sum;
+			}
+		}
+	}
+	
+	
+	
   
 }
 
